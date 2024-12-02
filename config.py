d2f905f22a14d895119424b0feffd1e16f9d3479
FRANKASEE <149130864+FRANKASEE@users.noreply.github.com>
2024-12-02 21:41:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,39 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
-    "https://raw.githubusercontent.com/suxuang/myIPTV/refs/heads/main/ipv6.m3u",
-    "https://raw.githubusercontent.com/FRANKASEE/iTV/refs/heads/main/iptv_list.txt",
-    "https://raw.githubusercontent.com/FRANKASEE/tviptv/refs/heads/Files/IPTVlive.m3u",
-    "https://raw.githubusercontent.com/FRANKASEE/iptv--/refs/heads/main/speedtest/zubo_fofa.txt",
-    "https://raw.githubusercontent.com/FRANKASEE/live-1/refs/heads/main/online.txt",
-    "https://raw.githubusercontent.com/FRANKASEE/live-1/refs/heads/main/iptv.txt",
-    "https://9295.kstore.space/ipv4.txt",
-    "http://tv.850930.xyz/kdsb.m3u",
-    "http://tv.850930.xyz/kdsb2.m3u",
-    "http://tv.850930.xyz/gather.m3u",
-    "https://raw.githubusercontent.com/xingzhe010203/xingzhe/refs/heads/main/IOS-V4.m3u",
+    "https://m3u.hackserver.net/txt/fmml_ipv6.txt",
+    "https://m3u.hackserver.net/txt/fmml_dv6.txt",
+    "https://raw.githubusercontent.com/YueChan/Live/main/APTV.m3u",
+    "https://m3u.hackserver.net/txt/y_g.txt",
+    "https://m3u.hackserver.net/txt/j_iptv.txt",
+    "https://raw.githubusercontent.com/n3rddd/CTVLive2/main/merged_output.m3u",
+    "https://raw.githubusercontent.com/n3rddd/CTVLive2/main/others_output.txt",
+    "http://175.178.251.183:6689/live.txt",
+    "https://raw.githubusercontent.com/redrainl/iptv/main/speedtest/zubo_fofa.txt",  # ADDED BY LEM ON 01/08/2024
+    "https://raw.githubusercontent.com/pxiptv/live/main/iptv.txt",  # ADDED BY LEM ON 08/08/2024
+    "http://120.79.4.185/new/mdlive.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
+    "https://live.zhoujie218.top/tv/iptv6.txt",
     "https://tv.youdu.fan:666/live/",
+    "http://ww.weidonglong.com/dsj.txt",
+    "http://xhztv.top/zbc.txt",
+    "https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt",
+    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+    "https://v.nxog.top/m/tv/1/",
+    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    "http://home.jundie.top:81/Cat/tv/live.txt",
+    "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+    "https://fm1077.serv00.net/SmartTV.m3u",
     "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
-    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt",
-    "http://ttkx.live:55/lib/kx2024.txt",
+    "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt",  # ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt",  # ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/yoursmile66/TVBox/main/live.txt",  # ADDED BY LEM ON 29/07/2024
+    "http://ttkx.live:55/lib/kx2024.txt",  # ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u",  # ADDED BY LEM ON 29/07/2024
+    "https://raw.githubusercontent.com/Love4vn/love4vn/main/Sport.m3u",  # 奥运 ON 29/07/2024
 ]
 
 url_blacklist = [
