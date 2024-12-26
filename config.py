02c2f1df8e6485e1f0cf9fd51dbc1b430e09610d
FRANKASEE <149130864+FRANKASEE@users.noreply.github.com>
2024-12-26 19:45:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ ip_version_priority = "ipv4"
 
 source_urls = [
     "https://m3u.hackserver.net/txt/fmml_ipv6.txt",
+    "https://raw.githubusercontent.com/FRANKASEE/TV/refs/heads/master/result.txt",
     "https://raw.githubusercontent.com/FRANKASEE/IPTV34/refs/heads/main/live.txt",
     "https://m3u.hackserver.net/txt/fmml_dv6.txt",
     "https://raw.githubusercontent.com/YueChan/Live/main/APTV.m3u",
